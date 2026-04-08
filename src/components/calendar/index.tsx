e77ef72c5ffadaf1affb50aaa66dc74d4bcdaf9f
Priyobroto Kar <priyobrotokar@gmail.com>
2026-04-08 21:45:46 +0530

    feat: add calendar binding rings

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 
 import { add, format, parse, sub } from 'date-fns';
+import Image from 'next/image';
 
 import { Button } from '../ui/button';
 import { CalendarDays } from './calendar-days';
@@ -30,10 +31,20 @@ function Calendar() {
   };
 
   return (
-    <div className="bg-card-background flex w-full max-w-2xl flex-col items-center shadow-2xl lg:w-fit lg:max-w-none lg:flex-row">
+    <div className="bg-card-background relative flex w-full max-w-2xl flex-col items-center shadow-2xl lg:w-fit lg:max-w-none lg:flex-row">
+      <div className="absolute -top-1 left-1/2 -translate-x-1/2">
+        <Image
+          src={'/binding.svg'}
+          alt="Calendar Binding"
+          width={660}
+          height={28}
+          className="min-w-64 sm:min-w-md lg:min-w-2xl"
+        />
+      </div>
+
       <HeroImage />
 
-      <div className="shrink-0 space-y-7 px-8 sm:px-20 lg:space-y-8">
+      <div className="shrink-0 space-y-7 px-6 sm:px-20 lg:space-y-8">
         <CalendarHeader firstDayOfMonth={firstDayOfMonth} />
         <div className="space-y-2 sm:space-y-4">
           <CalendarWeekdays />
